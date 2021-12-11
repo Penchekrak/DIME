a0ca47f4b295a2aa694e2c3d9438de8416a4221a
Max Nekrashevich <max.v.nekrashevich@gmail.com>
2021-12-11 19:53:34 +0300

    Added datamodules for other datasets.

--- datamodules.py
+++ datamodules.py
@@ -4,17 +4,49 @@ import pytorch_lightning as pl
 from hydra.utils import to_absolute_path
 from torch.utils.data import DataLoader
 from torchvision import transforms
-from torchvision.datasets import MNIST
+from torchvision.datasets import MNIST, CIFAR10, FashionMNIST
 
 
-class MNISTDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir: str = "path/to/dir", batch_size: int = 32):
+class DataModule(pl.LightningDataModule):
+    def __init__(self,
+                 data_dir: str = "path/to/dir",
+                 batch_size: int = 32,
+                 transform = transforms.ToTensor()):
         super().__init__()
         self.data_dir = to_absolute_path(data_dir)
         self.batch_size = batch_size
+        self.transform = transform
+        self.train_dataset = None
 
     def setup(self, stage: Optional[str] = None):
-        self.mnist_train = MNIST(self.data_dir, train=True, download=True, transform=transforms.ToTensor())
+        raise NotImplementedError
 
     def train_dataloader(self):
-        return DataLoader(self.mnist_train, batch_size=self.batch_size)
+        return DataLoader(self.train_dataset, batch_size=self.batch_size)
+
+
+class MNISTDataModule(DataModule):
+    def setup(self, stage: Optional[str] = None):
+        self.train_dataset = MNIST(self.data_dir,
+                                   train=True,
+                                   download=True,
+                                   transform=self.transform)
+
+
+class CIFAR10DataModule(DataModule):
+    def setup(self, stage: Optional[str] = None):
+        self.train_dataset = CIFAR10(self.data_dir,
+                                     train=True,
+                                     download=True,
+                                     transform=self.transform)
+
+
+class FashoinMNISTDataModule(DataModule):
+    def setup(self, stage: Optional[str] = None):
+        self.train_dataset = FashionMNIST(self.data_dir,
+                                          train=True,
+                                          download=True,
+                                          transform=self.transform)
+
+
+
